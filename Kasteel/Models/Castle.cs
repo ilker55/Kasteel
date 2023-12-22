7ba9f40996c3e92bf455bca6da842e5a3de9d449
Ilker <ilk.er_55@live.nl>
2023-12-22 17:24:15 +0100

    Created models

---
+++ Kasteel/Models/Castle.cs
@@ -0,0 +1,8 @@
+﻿namespace Kasteel.Models
+{
+    public class Castle(int id, string name)
+    {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+    }
+}
---
+++ Kasteel/Models/King.cs
@@ -0,0 +1,17 @@
+﻿namespace Kasteel.Models
+{
+    public class King(int id, int castleId, string name, string birthDate, string deathDate, string location, int year, string famousFor, string remarks)
+    {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string BirthDate { get; set; } = birthDate;
+        public string DeathDate { get; set; } = deathDate;
+        public string Location { get; set; } = location;
+        public int Year { get; set; } = year;
+        public string FamousFor { get; set; } = famousFor;
+        public string Remarks { get; set; } = remarks;
+
+        public int CastleId { get; set; } = castleId;
+        public Castle Castle { get; set; } = null!;
+    }
+}
