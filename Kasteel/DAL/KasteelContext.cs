a9381309531bce9d60f7c4d65fddf46a2f2e8217
Ilker <ilk.er_55@live.nl>
2023-12-22 17:24:58 +0100

    Added in-memory database with seeder from json files

---
+++ Kasteel/DAL/KasteelContext.cs
@@ -0,0 +1,33 @@
+﻿using Kasteel.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Kasteel.DAL
+{
+    public class KasteelContext(DbContextOptions<KasteelContext> options) : DbContext(options)
+    {
+        public virtual DbSet<Castle> Castles { get; set; } = null!;
+        public virtual DbSet<King> Kings { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed database
+            modelBuilder.Entity<Castle>().HasData(GetCastleSeedData());
+            modelBuilder.Entity<King>().HasData(GetKingSeedData());
+        }
+
+        private static Castle[] GetCastleSeedData()
+        {
+            using var r = new StreamReader("Files/kasteel.json");
+            return JsonConvert.DeserializeObject<Castle[]>(r.ReadToEnd()) ?? Array.Empty<Castle>();
+        }
+
+        private static King[] GetKingSeedData()
+        {
+            using var r = new StreamReader("Files/koning.json");
+            return JsonConvert.DeserializeObject<King[]>(r.ReadToEnd()) ?? Array.Empty<King>();
+        }
+    }
+}
--- Kasteel/Program.cs
+++ Kasteel/Program.cs
@@ -1,3 +1,6 @@
+using Kasteel.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<KasteelContext>(opt => opt.UseInMemoryDatabase("kasteel"));
 
 var app = builder.Build();
 
