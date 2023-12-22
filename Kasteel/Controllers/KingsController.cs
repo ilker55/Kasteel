7289b6105fad427448083f4606a6fa45e8456b5b
Ilker <ilk.er_55@live.nl>
2023-12-22 20:47:16 +0100

    Added kings controller for CRUD

---
+++ Kasteel/Controllers/KingsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Kasteel.DAL;
+using Kasteel.Models;
+
+namespace Kasteel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KingsController(KingRepository repository) : ControllerBase
+    {
+        private readonly IKingRepository _repository = repository;
+
+        // GET: api/Kings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<King>>> GetKings()
+            => await _repository.GetAll();
+
+        // GET: api/Kings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<King>> GetKing(int id)
+        {
+            var king = await _repository.GetByID(id);
+            return king == null
+                ? NotFound()
+                : king;
+        }
+
+        // PUT: api/Kings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutKing(int id, King king)
+        {
+            if (id != king.Id)
+                return BadRequest();
+
+            _repository.Update(king);
+
+            try
+            {
+                await _repository.Save();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Kings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<King>> PostKing(King king)
+        {
+            await _repository.Insert(king);
+            await _repository.Save();
+
+            return CreatedAtAction("GetKing", new { id = king.Id }, king);
+        }
+
+        // DELETE: api/Kings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKing(int id)
+        {
+            var deleted = await _repository.Delete(id);
+            if (!deleted)
+                return NotFound();
+
+            await _repository.Save();
+
+            return NoContent();
+        }
+    }
+}
